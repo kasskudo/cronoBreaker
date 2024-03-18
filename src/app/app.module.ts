74b09503f5fb3f90796cf7086e91af7fdd246119
David <daviddiez1990@gmail.com>
2024-03-18 23:38:35 +0100

    Squashed commit of the following:
    commit 4cdb2ca99dd4fcdf4952a1201bb2e69db78a988c
    Author: David <daviddiez1990@gmail.com>
    Date:   Mon Mar 18 23:35:51 2024 +0100
    
        Se crea el componente "usuario"
    
    commit d8981ed24b0bd6846794fcff6d0dbf2b2d6929ce
    Author: David <daviddiez1990@gmail.com>
    Date:   Mon Mar 18 23:34:39 2024 +0100
    
        Se crea el componente "login"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { UsuarioComponent } from './usuario/usuario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    LoginComponent,
+    UsuarioComponent
   ],
   imports: [
     BrowserModule,
